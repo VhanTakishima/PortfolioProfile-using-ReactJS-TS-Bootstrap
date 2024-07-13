84f6798c262826940a7196038589bbe6438924ff
Vhan Takishima <vhantakishima@gmail.com>
2024-07-14 04:23:17 +0800

    polished Rock paper scissors

--- frontend/src/component/RockPaperScissor.tsx
+++ frontend/src/component/RockPaperScissor.tsx
@@ -1,117 +1,168 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import "../styling/RockPaperScissor.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 type RockPaperScissorProps = {
   isVisible: boolean;
   onClose: () => void;
 };
-//to add reel funtion here for each button for starters
 
 function RockPaperScissor({ isVisible, onClose }: RockPaperScissorProps) {
   const [isResetVisible, setResetVisible] = useState(false);
   const [isPicksVisible, setPicksVisible] = useState(true);
+  const [playerChoice, setPlayerChoice] = useState("rock");
+  const [computerChoice, setComputerChoice] = useState("rock");
+  const [playerPoints, setPlayerPoints] = useState(0);
+  const [computerPoints, setComputerPoints] = useState(0);
+  const [resetRoundTimeout, setResetRoundTimeout] = useState(10);
+  const [resetState, setResetState] = useState(false);
 
-  const picks = ["rock", "paper", "scissor"];
+  useEffect(() => {
+    const timeOut = setInterval(() => {
+      setResetRoundTimeout(
+        (prevResetRoundTimeout) => prevResetRoundTimeout - 1
+      );
+    }, 1000);
+    return () => clearInterval(timeOut);
+  }, []);
+
+  useEffect(() => {
+    if (resetRoundTimeout === 0) {
+      setTimeout(() => {
+        setResetRoundTimeout(10);
+      }, 1000);
+      resetRollAll();
+    }
+  }, [resetRoundTimeout]);
+
+  useEffect(() => {
+    if (playerChoice && computerChoice && resetState) {
+      pointsHandler();
+    }
+  }, [playerChoice, computerChoice]);
+
+  const pointsHandler = () => {
+    if (playerChoice === computerChoice) {
+      console.log("It's a tie!");
+    } else if (
+      (playerChoice === "rock" && computerChoice === "scissor") ||
+      (playerChoice === "paper" && computerChoice === "rock") ||
+      (playerChoice === "scissor" && computerChoice === "paper")
+    ) {
+      setPlayerPoints((prevPoints) => prevPoints + 1);
+    } else if (playerChoice === "" || computerChoice === "") {
+      console.log("Clearing choices...");
+    } else {
+      setComputerPoints((prevPoints) => prevPoints + 1);
+    }
+  };
+
+  const choices = ["rock", "paper", "scissor"];
+  const toTitleCase = (str: string) => {
+    return str
+      .toLowerCase()
+      .split(" ")
+      .map((word: string) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  };
+
   const icon_width = 500,
-    icon_height = 411,
+    icon_height = 400,
     num_icons = 3,
     time_per_icon = 100;
 
-  const roll = (reel: HTMLElement, i?: number) => {
-    const delta = num_icons + Math.round(Math.random() * num_icons);
-    const style = getComputedStyle(reel);
-    const backgroundPositionY = parseFloat(style.backgroundPositionY);
+  const delta = num_icons + Math.round(Math.random() * num_icons);
+
+  const rollRock = (reel: HTMLElement) => {
     reel.style.transition = `background-position-y ${
       2 + delta * time_per_icon
     }ms`;
-    reel.style.backgroundPositionY = `${
-      backgroundPositionY + delta * icon_height
-    }px`;
+    // reel.style.backgroundPositionY = `173vh`;
+    reel.style.backgroundPositionY = "0%";
   };
 
-  const resetRoll = (reel: HTMLElement, i?: number) => {
-    const delta = num_icons + Math.round(Math.random() * num_icons);
-    const style = getComputedStyle(reel);
-    const backgroundPositionY = parseFloat(style.backgroundPositionY);
+  const rollPaper = (reel: HTMLElement) => {
     reel.style.transition = `background-position-y ${
       2 + delta * time_per_icon
     }ms`;
-    reel.style.backgroundPositionY = `0px`;
+    // reel.style.backgroundPositionY = `290vh`;
+    reel.style.backgroundPositionY = `50%`;
   };
 
-  function rollAll() {
-    const reelsCompList = document.querySelectorAll(".reel");
-    [...reelsCompList].map((reel, i) => {
-      console.log(reel, i);
-      roll(reel as HTMLElement, i);
-      setResetVisible(true);
-      setPicksVisible(false);
-    });
-  }
-
-  const clickRollRock = () => {
-    const rollRock = (reel: HTMLElement, i?: number) => {
-      const delta = num_icons + Math.round(Math.random() * num_icons);
-      const style = getComputedStyle(reel);
-      const backgroundPositionY = parseFloat(style.backgroundPositionY);
-      reel.style.transition = `background-position-y ${
-        2 + delta * time_per_icon
-      }ms`;
-      reel.style.backgroundPositionY = `2466px`;
-    };
-    const reelsCompList = document.querySelectorAll(".reelplayer");
-    [...reelsCompList].map((reel, i) => {
-      console.log(reel, i);
-      rollRock(reel as HTMLElement, i);
-      rollAll();
-    });
+  const rollScissor = (reel: HTMLElement) => {
+    reel.style.transition = `background-position-y ${
+      2 + delta * time_per_icon
+    }ms`;
+    // reel.style.backgroundPositionY = `405vh`;
+    reel.style.backgroundPositionY = "100%";
   };
 
-  const clickRollPaper = () => {
-    const rollPaper = (reel: HTMLElement, i?: number) => {
-      const delta = num_icons + Math.round(Math.random() * num_icons);
-      const style = getComputedStyle(reel);
-      const backgroundPositionY = parseFloat(style.backgroundPositionY);
-      reel.style.transition = `background-position-y ${
-        2 + delta * time_per_icon
-      }ms`;
-      reel.style.backgroundPositionY = `2055px`;
-    };
-    const reelsCompList = document.querySelectorAll(".reelplayer");
-    [...reelsCompList].map((reel, i) => {
-      console.log(reel, i);
-      rollPaper(reel as HTMLElement, i);
-      rollAll();
-    });
-  };
-  const clickRollScissor = () => {
-    const rollScissor = (reel: HTMLElement, i?: number) => {
-      const delta = num_icons + Math.round(Math.random() * num_icons);
-      const style = getComputedStyle(reel);
-      const backgroundPositionY = parseFloat(style.backgroundPositionY);
-      reel.style.transition = `background-position-y ${
-        2 + delta * time_per_icon
-      }ms`;
-      reel.style.backgroundPositionY = `1644px`;
-    };
-    const reelsCompList = document.querySelectorAll(".reelplayer");
-    [...reelsCompList].map((reel, i) => {
-      console.log(reel, i);
-      rollScissor(reel as HTMLElement, i);
-      rollAll();
-    });
+  const rollFunctions: {
+    [key: string]: (element: HTMLElement) => void;
+  } = {
+    rock: rollRock,
+    paper: rollPaper,
+    scissor: rollScissor,
   };
 
-  function resetRollAll() {
-    const reelsCompList = document.querySelectorAll(".reel");
-    [...reelsCompList].map((reel, i) => {
-      console.log(reel, i);
-      resetRoll(reel as HTMLElement, i);
-      setResetVisible(false);
-      setPicksVisible(true);
+  const generateComputerChoice = () => {
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    setComputerChoice(randomChoice);
+    return randomChoice;
+  };
+
+  const handleOnClick = (choice: string) => {
+    setPlayerChoice(choice);
+    const reelsPlayerList = document.querySelectorAll(".reelplayer");
+    [...reelsPlayerList].map((reel) => {
+      const rollFunction = rollFunctions[choice];
+      rollFunction(reel as HTMLElement);
     });
-  }
+
+    const compChoice = generateComputerChoice();
+    const reelsCompList = document.querySelectorAll(".reelcomputer");
+    [...reelsCompList].map((reel) => {
+      const rollFunction = rollFunctions[compChoice];
+      rollFunction(reel as HTMLElement);
+    });
+
+    setResetVisible(true);
+    setPicksVisible(false);
+    console.log({ playerChoice: choice }, { computerChoice: compChoice });
+    console.log(playerPoints);
+    console.log(computerPoints);
+    setResetState(true);
+  };
+
+  const resetRollAll = () => {
+    const reelsCompList = document.querySelectorAll(".reelcomputer");
+    [...reelsCompList].map((reel) => {
+      if (reel instanceof HTMLElement) {
+        reel.style.transition = `background-position-y ${
+          2 + delta * time_per_icon
+        }ms`;
+        reel.style.backgroundPositionY = `0px`;
+      }
+    });
+
+    const reelsPlayerList = document.querySelectorAll(".reelplayer");
+    [...reelsPlayerList].map((reel) => {
+      if (reel instanceof HTMLElement) {
+        reel.style.transition = `background-position-y ${
+          2 + delta * time_per_icon
+        }ms`;
+        reel.style.backgroundPositionY = `0px`;
+      }
+    });
+
+    setResetVisible(false);
+    setPicksVisible(true);
+    setPlayerChoice("");
+    setComputerChoice("");
+    setResetRoundTimeout(10);
+    setResetState(false);
+  };
 
   return (
     <form className={isVisible ? "" : "hidden"}>
@@ -120,43 +171,76 @@ function RockPaperScissor({ isVisible, onClose }: RockPaperScissorProps) {
           type="button"
           value="Close"
           onClick={onClose}
-          className=" btn btn-primary"
+          className="btn btn-primary w-100"
         >
           X
         </button>
-        <div className="titlearea row">Rock Ppaper scissor</div>
+
+        <div className="titlearea row">
+          <h4 className="col-4">Rock Paper Scissor</h4>
+          <h4 className="col-4 text-center">Player's Points: {playerPoints}</h4>
+          <h4 className="col-3 text-center">AI's Points: {computerPoints}</h4>
+        </div>
         <div className="gamearea row">
           <div className="selector col-4">
             <div className="row">
-              <h4>Choose your pick!!</h4>
+              <h5 className="text-center">Choose your pick!!</h5>
+
               {isPicksVisible && (
                 <>
-                  <button className=" btn btn-primary" onClick={clickRollRock}>
-                    Rock
-                  </button>
-                  <button className=" btn btn-primary" onClick={clickRollPaper}>
-                    Paper
-                  </button>
-                  <button
-                    className=" btn btn-primary"
-                    onClick={clickRollScissor}
-                  >
-                    Scissor
-                  </button>
+                  {choices.map((choice, index) => (
+                    <button
+                      className="btn btn-primary"
+                      key={index}
+                      onClick={() => handleOnClick(choice)}
+                    >
+                      {toTitleCase(choice)}
+                    </button>
+                  ))}
                 </>
               )}
               {!isPicksVisible && (
-                <button className=" btn btn-primary" onClick={resetRollAll}>
-                  Reset
-                </button>
+                <>
+                  <button className="btn btn-primary" onClick={resetRollAll}>
+                    Reset Now
+                  </button>
+                  <p className="text-center">
+                    {" "}
+                    Round will reset in: {resetRoundTimeout}s
+                  </p>
+                </>
               )}
+              <div className="row h-auto">
+                <div className="col-12 text-center">
+                  {window.innerWidth > 1199 && (
+                    <div className="d-flex flex-column align-items-center mt-4">
+                      <p>
+                        Mechanics: <br /> Rock beats Paper,Paper beats Scissors,
+                        Scissor beats Rock. Simple as that.
+                      </p>
+                    </div>
+                  )}
+                </div>
+              </div>
             </div>
           </div>
           <div className="reelareaplayer col-4 p-0">
             <div className="reelplayer h-100"></div>
           </div>
-          <div className="reelareacomp  col-4 p-0">
-            <div className="reel h-100"></div>
+          <div className="reelareacomp col-4 p-0">
+            <div className="reelcomputer h-100"></div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 text-center">
+            {window.innerWidth < 1199 && (
+              <div>
+                <p>
+                  Mechanics: Rock beats Paper,Paper beats Scissors, Scissor
+                  beats Rock. Simple as that.
+                </p>
+              </div>
+            )}
           </div>
         </div>
       </div>
