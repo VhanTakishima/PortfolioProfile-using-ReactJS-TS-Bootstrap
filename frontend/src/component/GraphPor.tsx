cc633569bdb63301ef40c45db11c60ee221eb154
Vhan Takishima <vhantakishima@gmail.com>
2024-07-15 12:02:15 +0800

    will continue graphpor

--- frontend/src/component/GraphPor.tsx
+++ frontend/src/component/GraphPor.tsx
@@ -1,4 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from "react";
+import { Modal, Button, Image } from "react-bootstrap";
+
 import "../styling/GraphPor.css";
 
 type GraphPorProps = {
@@ -7,140 +10,238 @@ type GraphPorProps = {
 };
 
 function GraphPor({ isVisible, onClose }: GraphPorProps) {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const handleImageClick = (src: string) => {
+    setSelectedImage(src);
+  };
+
+  const closeModal = () => {
+    setSelectedImage(null);
+  };
+
   return (
-    <form className={isVisible ? "" : "hidden"}>
-      <div className="graphpordisplay col">
-        <div className="row">
-          <div className="col-1 ">
-            <button
-              type="button"
-              value="Close"
-              onClick={onClose}
-              className=" btn btn-primary"
-            >
-              X
-            </button>
+    <>
+      <form className={isVisible ? "" : "hidden"}>
+        <div className="graphpordisplay col">
+          <div className="row">
+            <div className="col-1">
+              <Button variant="primary" onClick={onClose}>
+                X
+              </Button>
+            </div>
           </div>
-          <div className="col-11">
-            These section is my previous work for the players who hired me in
-            the game Torn.com
+
+          <div className="galleryarea">
+            <div className="row pt-3">
+              These section is my previous work for the players who hired me in
+              the game Torn.com
+            </div>
+
+            <div className="row pt-3 align-content-center">
+              <img
+                src="https://i.ibb.co/vkYMTQG/profile-signe-vhan.gif"
+                className="img-fluid rounded float-start col-6"
+                alt=""
+                onClick={() =>
+                  handleImageClick(
+                    "https://i.ibb.co/vkYMTQG/profile-signe-vhan.gif"
+                  )
+                }
+              />
+              <img
+                src="https://i.ibb.co/G0rK7CT/proudmember-FINAL.gif"
+                className="img-fluid rounded float-start col-6"
+                alt=""
+                onClick={() =>
+                  handleImageClick(
+                    "https://i.ibb.co/G0rK7CT/proudmember-FINAL.gif"
+                  )
+                }
+              />
+            </div>
+            <div className="row pt-3 align-content-center">
+              <img
+                src="https://i.ibb.co/Xxs03hH/Landscape-final.gif"
+                className="img-fluid rounded float-start col-3"
+                alt=""
+                onClick={() =>
+                  handleImageClick(
+                    "https://i.ibb.co/Xxs03hH/Landscape-final.gif"
+                  )
+                }
+              />
+              <img
+                src="https://i.ibb.co/ZJg5H8G/finasl-1.gif"
+                className="img-fluid rounded float-start col-5"
+                alt=""
+                onClick={() =>
+                  handleImageClick("https://i.ibb.co/ZJg5H8G/finasl-1.gif")
+                }
+              />
+              <img
+                src="https://i.ibb.co/WV1jgdP/ssdd.gif"
+                className="img-fluid rounded float-start col-4"
+                alt=""
+                onClick={() =>
+                  handleImageClick("https://i.ibb.co/WV1jgdP/ssdd.gif")
+                }
+              />
+            </div>
+            <div className="row pt-3 align-content-center">
+              <img
+                src="https://i.ibb.co/5TBMQgM/REDID-1.gif"
+                className="img-fluid rounded float-start col-3"
+                alt=""
+                onClick={() =>
+                  handleImageClick("https://i.ibb.co/5TBMQgM/REDID-1.gif")
+                }
+              />
+              <img
+                src="https://i.ibb.co/3fTcMtJ/final.gif"
+                className="img-fluid rounded float-start col-3"
+                alt=""
+                onClick={() =>
+                  handleImageClick("https://i.ibb.co/3fTcMtJ/final.gif")
+                }
+              />
+              <img
+                src="https://i.ibb.co/M7HkvTJ/optimized-watermarked.gif"
+                className="img-fluid rounded float-start col-3"
+                alt=""
+                onClick={() =>
+                  handleImageClick(
+                    "https://i.ibb.co/M7HkvTJ/optimized-watermarked.gif"
+                  )
+                }
+              />
+              <img
+                src="https://i.ibb.co/MZGwjKX/Animated-w-watermark.gif"
+                className="img-fluid rounded float-start col-3"
+                alt=""
+                onClick={() =>
+                  handleImageClick(
+                    "https://i.ibb.co/MZGwjKX/Animated-w-watermark.gif"
+                  )
+                }
+              />
+            </div>
+            <div className="row pt-3 align-content-center">
+              <img
+                src="https://i.ibb.co/vxvTjNV/Browser-Preview-tmp.gif"
+                className="img-fluid rounded float-start col-3"
+                alt=""
+                onClick={() =>
+                  handleImageClick(
+                    "https://i.ibb.co/vxvTjNV/Browser-Preview-tmp.gif"
+                  )
+                }
+              />
+              <img
+                src="https://i.ibb.co/xhryTqv/neonwith-watermark2.gif"
+                className="img-fluid rounded float-start col-3"
+                alt=""
+                onClick={() =>
+                  handleImageClick(
+                    "https://i.ibb.co/xhryTqv/neonwith-watermark2.gif"
+                  )
+                }
+              />
+              <img
+                src="https://i.ibb.co/Czq2nqv/PREVIEW.gif"
+                className="img-fluid rounded float-start col-3"
+                alt=""
+                onClick={() =>
+                  handleImageClick("https://i.ibb.co/Czq2nqv/PREVIEW.gif")
+                }
+              />
+              <img
+                src="https://i.ibb.co/q5R4bRY/Final.gif"
+                className="img-fluid rounded float-start col-3"
+                alt="https://i.ibb.co/LR5DZnC/with-watermark.gif"
+                onClick={() =>
+                  handleImageClick("https://i.ibb.co/q5R4bRY/Final.gif")
+                }
+              />
+            </div>
+            <div className="row pt-3 align-content-center">
+              <img
+                src="https://i.ibb.co/2kBKGTm/wip.gif"
+                className="img-fluid rounded float-start col-3"
+                alt=""
+                onClick={() =>
+                  handleImageClick("https://i.ibb.co/2kBKGTm/wip.gif")
+                }
+              />
+              <img
+                src="https://i.ibb.co/WkSBwV8/Final-nbanner.gif"
+                className="img-fluid rounded float-start col-3"
+                alt=""
+                onClick={() =>
+                  handleImageClick("https://i.ibb.co/WkSBwV8/Final-nbanner.gif")
+                }
+              />
+              <img
+                src="https://i.ibb.co/Xk51jRT/profile-sig.png"
+                className="img-fluid rounded float-start col-6"
+                alt=""
+                onClick={() =>
+                  handleImageClick("https://i.ibb.co/Xk51jRT/profile-sig.png")
+                }
+              />
+            </div>
+            <div className="row pt-3 align-content-center">
+              <img
+                src="https://i.ibb.co/Zf34K7s/Final.gif"
+                className="img-fluid rounded float-start col-3"
+                alt=""
+                onClick={() =>
+                  handleImageClick("https://i.ibb.co/Zf34K7s/Final.gif")
+                }
+              />
+              <img
+                src="https://i.ibb.co/sJv6F10/resized-final.gif"
+                className="img-fluid rounded float-start col-3"
+                alt=""
+                onClick={() =>
+                  handleImageClick("https://i.ibb.co/sJv6F10/resized-final.gif")
+                }
+              />
+              <img
+                src="https://i.ibb.co/pz95WxG/watermarked-2.gif"
+                className="img-fluid rounded float-start col-3"
+                alt=""
+                onClick={() =>
+                  handleImageClick("https://i.ibb.co/pz95WxG/watermarked-2.gif")
+                }
+              />
+              <img
+                src="https://i.ibb.co/4JpkskC/MHC-Main-1.gif"
+                className="img-fluid rounded float-start col-3"
+                alt=""
+                onClick={() =>
+                  handleImageClick("https://i.ibb.co/4JpkskC/MHC-Main-1.gif")
+                }
+              />
+            </div>
           </div>
         </div>
 
-        <div className="row pt-3 align-content-center">
-          <img
-            src="https://i.ibb.co/vkYMTQG/profile-signe-vhan.gif"
-            className="img-fluid rounded float-start col-6"
-            alt=""
-          />
-          <img
-            src="https://i.ibb.co/G0rK7CT/proudmember-FINAL.gif"
-            className="img-fluid rounded float-start col-6"
-            alt=""
-          />
-        </div>
-        <div className="row pt-3 align-content-center">
-          <img
-            src="https://i.ibb.co/Xxs03hH/Landscape-final.gif"
-            className="img-fluid rounded float-start col-3"
-            alt=""
-          />
-          <img
-            src="https://i.ibb.co/ZJg5H8G/finasl-1.gif"
-            className="img-fluid rounded float-start col-5"
-            alt=""
-          />
-          <img
-            src="https://i.ibb.co/WV1jgdP/ssdd.gif"
-            className="img-fluid rounded float-start col-4"
-            alt=""
-          />
-        </div>
-        <div className="row pt-3 align-content-center">
-          <img
-            src="https://i.ibb.co/5TBMQgM/REDID-1.gif"
-            className="img-fluid rounded float-start col-3"
-            alt=""
-          />
-          <img
-            src="https://i.ibb.co/3fTcMtJ/final.gif"
-            className="img-fluid rounded float-start col-3"
-            alt=""
-          />
-          <img
-            src="https://i.ibb.co/M7HkvTJ/optimized-watermarked.gif"
-            className="img-fluid rounded float-start col-3"
-            alt=""
-          />
-          <img
-            src="https://i.ibb.co/MZGwjKX/Animated-w-watermark.gif"
-            className="img-fluid rounded float-start col-3"
-            alt=""
-          />
-        </div>
-        <div className="row pt-3 align-content-center">
-          <img
-            src="https://i.ibb.co/vxvTjNV/Browser-Preview-tmp.gif"
-            className="img-fluid rounded float-start col-3"
-            alt=""
-          />
-          <img
-            src="https://i.ibb.co/xhryTqv/neonwith-watermark2.gif"
-            className="img-fluid rounded float-start col-3"
-            alt=""
-          />
-          <img
-            src="https://i.ibb.co/Czq2nqv/PREVIEW.gif"
-            className="img-fluid rounded float-start col-3"
-            alt=""
-          />
-          <img
-            src="https://i.ibb.co/q5R4bRY/Final.gif"
-            className="img-fluid rounded float-start col-3"
-            alt="https://i.ibb.co/LR5DZnC/with-watermark.gif"
-          />
-        </div>
-        <div className="row pt-3 align-content-center">
-          <img
-            src="https://i.ibb.co/2kBKGTm/wip.gif"
-            className="img-fluid rounded float-start col-3"
-            alt=""
-          />
-          <img
-            src="https://i.ibb.co/WkSBwV8/Final-nbanner.gif"
-            className="img-fluid rounded float-start col-3"
-            alt=""
-          />
-          <img
-            src="https://i.ibb.co/Xk51jRT/profile-sig.png"
-            className="img-fluid rounded float-start col-6"
-            alt=""
-          />
-        </div>
-        <div className="row pt-3 align-content-center">
-          <img
-            src="https://i.ibb.co/Zf34K7s/Final.gif"
-            className="img-fluid rounded float-start col-3"
-            alt=""
-          />
-          <img
-            src="https://i.ibb.co/sJv6F10/resized-final.gif"
-            className="img-fluid rounded float-start col-3"
-            alt=""
-          />
-          <img
-            src="https://i.ibb.co/pz95WxG/watermarked-2.gif"
-            className="img-fluid rounded float-start col-3"
-            alt=""
-          />
-          <img
-            src="https://i.ibb.co/4JpkskC/MHC-Main-1.gif"
-            className="img-fluid rounded float-start col-3"
-            alt=""
-          />
-        </div>
-      </div>
-    </form>
+        <Modal show={!!selectedImage} onHide={closeModal} centered>
+          <Modal.Header closeButton></Modal.Header>
+          <Modal.Body>
+            {selectedImage && (
+              <Image
+                className=" modalimg"
+                src={selectedImage}
+                alt="Fullscreen"
+                fluid
+              />
+            )}
+          </Modal.Body>
+        </Modal>
+      </form>
+    </>
   );
 }
 
